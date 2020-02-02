365d8dfbf6c7e2bd13a66c15445ae19f1e061e69
amoghwagh <amoghwagh@gmail.com>
2020-02-02 19:42:20 +0530

    Added json placeholder to retrieve information

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
 import AddTodo from "./components/AddTodo";
+import axios from "axios";
+
 export default {
   name: "app",
   components: {
@@ -19,23 +21,24 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: false
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
+      todos: []
+      // todos: [
+      //   {
+      //     id: 1,
+      //     title: "Todo One",
+      //     completed: false
+      //   },
+      //   {
+      //     id: 2,
+      //     title: "Todo Two",
+      //     completed: false
+      //   },
+      //   {
+      //     id: 3,
+      //     title: "Todo Three",
+      //     completed: false
+      //   }
+      // ]
     };
   },
   methods: {
@@ -45,6 +48,14 @@ export default {
     addTodo(newTodo) {
       this.todos = [...this.todos, newTodo];
     }
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(res => {
+        this.todos = res.data;
+      })
+      .catch(error => console.log(error));
   }
 };
 </script>
