e99a79b3ff73e4fb2c76c4eb4eb1a394a3f5674b
amoghwagh <amoghwagh@gmail.com>
2020-02-02 16:48:12 +0530

    Added Header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <Header />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
 export default {
   name: "app",
   components: {
-    Todos
+    Todos,
+    Header
   },
   data() {
     return {
@@ -47,6 +50,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 6px;
 }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="header">
+    <h1>Todo List</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 2px;
+  margin: 0;
+}
+
+.header a {
+  color: #fff;
+  padding-right: 5px;
+}
+</style>
