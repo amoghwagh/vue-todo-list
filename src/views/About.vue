39ef12c7a1f8e6c26a01b0a387ead037e7ca36d1
amoghwagh <amoghwagh@gmail.com>
2020-02-02 19:56:52 +0530

    Changed the About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>
+      This is the Todo List app which was built to learn the basics of VueJs
+    </h1>
   </div>
 </template>
