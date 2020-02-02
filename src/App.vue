305a7f1a9f8c66fac7bc35a32efde21d79c2bed1
amoghwagh <amoghwagh@gmail.com>
2020-02-02 19:45:39 +0530

    Added vue router

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,32 @@
 <template>
   <div id="app">
-    <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Todos from "./components/Todos";
-import Header from "./components/layout/Header";
-import AddTodo from "./components/AddTodo";
-import axios from "axios";
-
-export default {
-  name: "app",
-  components: {
-    Todos,
-    Header,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: []
-      // todos: [
-      //   {
-      //     id: 1,
-      //     title: "Todo One",
-      //     completed: false
-      //   },
-      //   {
-      //     id: 2,
-      //     title: "Todo Two",
-      //     completed: false
-      //   },
-      //   {
-      //     id: 3,
-      //     title: "Todo Three",
-      //     completed: false
-      //   }
-      // ]
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(item => item.id !== id);
-    },
-    addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
-    }
-  },
-  created() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
-      .then(res => {
-        this.todos = res.data;
-      })
-      .catch(error => console.log(error));
-  }
-};
-</script>
-
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 6px;
 }
 
-.btn {
-  display: inline-block;
-  border: none;
-  background: #555;
-  color: #fff;
-  padding: 7px, 20px;
-  cursor: pointer;
+#nav {
+  padding: 30px;
 }
 
-.btn:hover {
-  background: #666;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
